2a1e9fca34dece0ef44e61d7cba1652a3de9ce12
sbaysting <samuelbaysting@gmail.com>
2015-03-21 15:45:16 -0400

    Added more example orders to ExampleOrders.java

--- ExampleOrders.java
+++ ExampleOrders.java
@@ -6,6 +6,7 @@ public class ExampleOrders {
 	public TableOrder table2;
 	public TableOrder table3;
 	public TableOrder table4;
+	public TableOrder table5;
 	
 	public ExampleOrders()
 	{
@@ -15,6 +16,7 @@ public class ExampleOrders {
 		table2 = new TableOrder(emp,2);
 		table4 = new TableOrder(emp,4);
 		table3 = new TableOrder(emp,3);
+		table5 = new TableOrder(emp,5);
 		
 		//table1.add(new Order(int MENU_ID, int Quantity, String SPC_REQ, int seatNumber));
 		table1.add(new Order(1, 1, null,1));
@@ -23,5 +25,13 @@ public class ExampleOrders {
 		table2.add(new Order(2, 2, null,1));
 		table2.add(new Order(5, 2, "NO FRIES", 3));
 		table2.add(new Order(6, 1, null, 2));
+		table3.add(new Order(3, 2, null, 4));
+		table3.add(new Order(1, 1, null, 3));
+		table3.add(new Order(1, 1, null, 1));
+		table3.add(new Order(2, 3, null, 2));
+		table4.add(new Order(6, 1, null, 1));
+		table4.add(new Order(4, 1, null, 1));
+		table5.add(new Order(5, 4, null, 1));
+		table5.add(new Order(2, 2, null, 2));
 	}
 }
