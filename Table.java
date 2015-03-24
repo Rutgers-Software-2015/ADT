0cd27b59df437cb6eaaa05496de9a5be9b7b379d
thetandoori <Rahul@nbp-199-48.nbp.ruw.rutgers.edu>
2015-03-23 22:13:05 -0400

    Added Table ADT
    I have added an ADT for Table.

---
+++ Table.java
@@ -0,0 +1,17 @@
+package ADT;
+
+public class Table 
+{
+	public int TABLE_ID;
+	public Employee Employee;
+	public boolean Table_Status;
+	
+	public Table(int ID,Employee E,boolean status)
+	{
+		TABLE_ID=ID;
+		Employee=E;
+		Table_Status=status;
+	}
+	
+	
+}
