bc393732cafd12f32482370c2c050a4cc0889a25
sbaysting <samuelbaysting@gmail.com>
2015-03-23 22:39:15 -0400

    Added templates for future functions

--- Employee.java
+++ Employee.java
@@ -7,6 +7,8 @@ public class Employee
 	public String Position;
 	public String Salary;
 	public String ID;
+	private String Username;
+	private String Password;
 	
 
 	public Employee()
@@ -27,6 +29,23 @@ public class Employee
 		ID = ident;
 	}
 	
+	public boolean setEmployeeUsername(String newUser)
+	{
+		//Must be logged in as this employee to use or be a Manager
+		return true;
+	}
+	
+	public String getEmployeeUsername()
+	{
+		//Can only be accessed by Manager or Login GUI
+		return null;
+	}
+	
+	public String getEmployeePassword()
+	{
+		//Can only be accessed by the Login GUI (not even the Manager)
+		return null;
+	}
 	
 
 }
