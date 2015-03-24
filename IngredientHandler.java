8f11608e646d207d4d4bf0f48dc8495077673a5c
thetandoori <Rahul@nbp-199-48.nbp.ruw.rutgers.edu>
2015-03-24 00:03:26 -0400

    Added variables and functions
    Used by manager and will link with the KS GUI

--- IngredientHandler.java
+++ IngredientHandler.java
@@ -5,17 +5,40 @@ import java.util.Vector;
 
 public class IngredientHandler 
 {
-	public Vector<Ingredient> IngredientList;
+	public static Ingredient IngredientList[]={
+	new Ingredient("Tomatoes",500),
+	new Ingredient("Chickens",500),
+	new Ingredient("Patatoes",500),
+	new Ingredient("Melons",500),
+	new Ingredient("Cheese",500)};
 	
 
-	public void IngredientHandler()
-	{
-		IngredientList=new Vector<Ingredient>();
-	}
 	public void AddIngredient(String name,int count)
-	{
-		IngredientList.add(new Ingredient(name,count));
+	{	
+		Ingredient temp[] = new Ingredient[IngredientList.length +1];
+	
+		for(int j = 0; j < IngredientList.length ; j++)
+		{
+			temp[j] = IngredientList[j];
+		}
+		
+		temp[temp.length - 1] = new Ingredient(name,count);
 
 	}
 	
+	public void UpdateInventory(Ingredient I,int added)
+	{
+		int update=-1;
+		for(int i=0;i<IngredientList.length;i++)
+		{
+			if(I.equals(IngredientList[i]))
+			{
+				update=i;
+				break;
+			}
+		}
+		
+		IngredientList[update].count=IngredientList[update].count +added;
+		
+	}
 }
